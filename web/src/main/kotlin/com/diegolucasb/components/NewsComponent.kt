b3c7f657fc06b12b563d71826eff552c4e9cdec7
Lucas Diego Amaral <diegolucasb@gmail.com>
2018-11-16 00:11:20 -0200

    Adds web news component and index file

--- web/src/main/kotlin/com/diegolucasb/components/NewsComponent.kt
+++ web/src/main/kotlin/com/diegolucasb/components/NewsComponent.kt
@@ -1,4 +1,50 @@
 package com.diegolucasb.components
 
-class NewsComponent {
+import com.diegolucasb.data.News
+import com.diegolucasb.presentation.NewsPresenter
+import com.diegolucasb.repository.NewsRepositoryImpl
+import com.diegolucasb.view.NewsView
+import kotlinx.html.js.onClickFunction
+import react.*
+import react.dom.button
+import react.dom.div
+import react.dom.link
+
+class NewsComponent: RComponent<RProps, ManagerComponentState>(), NewsView {
+
+    private val repository = NewsRepositoryImpl()
+    private val presenter = NewsPresenter(this, repository)
+
+    override fun componentDidMount() {
+        presenter.onStart()
+    }
+
+    override fun RBuilder.render() {
+        div(classes = "center") {
+            val news = state.news
+            if (news == null) {
+                +"There is no news"
+            } else {
+                div(classes = "news-container") {
+                    div(classes = "text") { +news.text
+//                        link { +news.link }
+                    }
+                    div(classes = "source") { +news.source }
+                    button(classes = "next") {
+                        +"Next"
+                        attrs { onClickFunction = { presenter.onNext() } }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun showNews(news: News) {
+        setState { this.news = news }
+
+    }
+}
+
+external interface ManagerComponentState : RState {
+    var news: News?
 }
--- web/src/main/kotlin/com/diegolucasb/main.kt
+++ web/src/main/kotlin/com/diegolucasb/main.kt
@@ -1,6 +1,6 @@
-package com.marcinmoskala
+package com.diegolucasb
 
-import com.marcinmoskala.components.QuotesComponent
+import com.diegolucasb.components.NewsComponent
 import react.dom.render
 import kotlin.browser.document
 import kotlin.browser.window
@@ -8,7 +8,7 @@ import kotlin.browser.window
 fun main(args: Array<String>) {
     window.onload = {
         render(document.getElementById("root")!!) {
-            child(QuotesComponent::class) {}
+            child(NewsComponent::class) {}
         }
     }
 }
